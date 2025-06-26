8461da8fa52ba8783eb7ad37ed0e790ea2c93011
Jonathan Hoyt <jonmagic@gmail.com>
2025-06-25 21:21:16 -0700

    fix: ensure .nojekyll file is created after build
    - Add .nojekyll file to the output directory after build to prevent GitHub Pages from ignoring files/folders starting with an underscore.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const fs = require("fs");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.on('afterBuild', () => {
+    fs.writeFileSync('_site/.nojekyll', '');
+  });
   return {
     dir: {
       input: "src",
