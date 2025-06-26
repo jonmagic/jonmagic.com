41bd1820f08d2e747b70e341963119fa9a1ce738
Jonathan Hoyt <jonmagic@gmail.com>
2025-06-25 20:02:23 -0700

    feat: add footer with copyright and year, update year with javascript

---
+++ js/site.js
@@ -0,0 +1,8 @@
+
+// Set the current year in the footer
+document.addEventListener('DOMContentLoaded', function() {
+  var yearSpan = document.getElementById('year');
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
